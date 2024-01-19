d10bc53f6a6dc4a44808917101037cd67771716c
Aman Pandia <pandiaaman16@gmail.com>
2024-01-20 00:32:26 +0530

    First commit for spring core project

---
+++ springcore2024/src/main/java/com/pandiaaman/springcore2024/App.java
@@ -0,0 +1,13 @@
+package com.pandiaaman.springcore2024;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
