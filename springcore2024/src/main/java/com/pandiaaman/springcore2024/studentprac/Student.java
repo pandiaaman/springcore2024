ed3deadcae77d62806c3f7973929a77bd6507b01
Aman Pandia <pandiaaman16@gmail.com>
2024-01-20 09:11:25 +0530

    collection injection in spring

--- springcore2024/src/main/java/com/pandiaaman/springcore2024/studentprac/Student.java
+++ springcore2024/src/main/java/com/pandiaaman/springcore2024/studentprac/Student.java
@@ -1,10 +1,23 @@
 package com.pandiaaman.springcore2024.studentprac;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
 public class Student {
 	
 	private int studentId;
 	
 	private String studentName;
+	
+	private List<String> studentMarks;
+	
+	private Set<String> studentPhones;
+	
+	private Map<String, String> studentOlderSchools;
+	
+	private Properties studentProps;
 	
 	private String studentAddress;
 	
@@ -13,12 +26,25 @@ public class Student {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Student(int studentId, String studnetName, String studentAddress) {
+	public Student(int studentId, String studentName, String studentAddress) {
 		super();
 		this.studentId = studentId;
 		this.studentName = studentName;
 		this.studentAddress = studentAddress;
 	}
+	
+	public Student(int studentId, String studentName, List<String> studentMarks, Set<String> studentPhones,
+			Map<String, String> studentOlderSchools, String studentAddress, Properties props) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentMarks = studentMarks;
+		this.studentPhones = studentPhones;
+		this.studentOlderSchools = studentOlderSchools;
+		this.studentAddress = studentAddress;
+		this.studentProps = props;
+	}
+
 
 	public int getStudentId() {
 		return studentId;
@@ -43,11 +69,44 @@ public class Student {
 	public void setStudentAddress(String studentAddress) {
 		this.studentAddress = studentAddress;
 	}
+	
+	public List<String> getStudentMarks() {
+		return studentMarks;
+	}
+
+	public void setStudentMarks(List<String> studentMarks) {
+		this.studentMarks = studentMarks;
+	}
+
+	public Set<String> getStudentPhones() {
+		return studentPhones;
+	}
+
+	public void setStudentPhones(Set<String> studentPhones) {
+		this.studentPhones = studentPhones;
+	}
+
+	public Map<String, String> getStudentOlderSchools() {
+		return studentOlderSchools;
+	}
+	
+	public Properties getStudentProps() {
+		return studentProps;
+	}
+
+	public void setStudentProps(Properties studentProps) {
+		this.studentProps = studentProps;
+	}
+
+	public void setStudentOlderSchools(Map<String, String> studentOlderSchools) {
+		this.studentOlderSchools = studentOlderSchools;
+	}
 
 	@Override
 	public String toString() {
-		return "Student [studentId=" + studentId + ", studnetName=" + studentName + ", studentAddress=" + studentAddress
-				+ "]";
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentMarks=" + studentMarks
+				+ ", studentPhones=" + studentPhones + ", studentOlderSchools=" + studentOlderSchools
+				+ ", studentProps=" + studentProps + ", studentAddress=" + studentAddress + "]";
 	}
 	
 	
